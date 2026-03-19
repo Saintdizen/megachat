a65c7f6641332425c79bc83e5563481568780875
chuvakhin <saintdizen@gmail.com>
2026-03-19 12:21:32 +0300

    master v11

--- public/client.js
+++ public/client.js
@@ -1,4 +1,4 @@
-const socket = io.connect('http://localhost:8080');
+const socket = io.connect('https://chuichat.ru/');
 const localVideo = document.getElementById('localVideo');
 const remoteVideo = document.getElementById('remoteVideo');
 let currentPeer = null;
